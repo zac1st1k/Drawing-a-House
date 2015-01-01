ae145b1ee68cf229ccf8d1c2882503ef6ec04279
zac1st1k <zacfirst@gmail.com>
2015-01-01 17:58:21 +1100

    Initial Commit

---
+++ Drawing a House/XZZViewController.h
@@ -0,0 +1,13 @@
+//
+//  XZZViewController.h
+//  Drawing a House
+//
+//  Created by Zac on 1/01/2015.
+//  Copyright (c) 2015 1st1k. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XZZViewController : UIViewController
+
+@end
---
+++ Drawing a House/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Drawing a House
+//
+//  Created by Zac on 1/01/2015.
+//  Copyright (c) 2015 1st1k. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XZZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XZZAppDelegate class]));
+    }
+}
